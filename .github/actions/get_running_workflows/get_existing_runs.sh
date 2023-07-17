3a366fb0bb7ce93dd22c2bbfa4f57d5d559c2849
Nicholas <nichlai02@gmail.com>
2023-07-17 15:09:03 +0800

    test false

--- .github/actions/get_running_workflows/get_existing_runs.sh
+++ .github/actions/get_running_workflows/get_existing_runs.sh
@@ -2,7 +2,7 @@ mapfile -t all_runs < all_runs.txt
 echo "existing_runs=false" >> "$GITHUB_OUTPUT"
 for run_number in $all_runs
 do
-    if [ $run_number -eq $CURRENT_RUN_NUMBER ]
+    if [ $run_number -lt $CURRENT_RUN_NUMBER ]
     then
         echo "existing_runs=true" >> "$GITHUB_OUTPUT"
     fi
