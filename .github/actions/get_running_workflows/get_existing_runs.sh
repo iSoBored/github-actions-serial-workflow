cbacfea7ed76487b313adfbc0b88b1b97ff44182
Nicholas <nichlai02@gmail.com>
2023-07-17 15:30:03 +0800

    more info

--- .github/actions/get_running_workflows/get_existing_runs.sh
+++ .github/actions/get_running_workflows/get_existing_runs.sh
@@ -1,9 +1,9 @@
 mapfile -t all_runs < all_runs.txt
 echo "existing_runs=false" >> "$GITHUB_OUTPUT"
-echo $CURRENT_RUN_NUMBER
+echo "Current Run Number: $CURRENT_RUN_NUMBER"
 for run_number in $all_runs
 do
-    echo $run_number
+    echo "For Loop Run Number: $run_number"
     if [ $run_number -lt $CURRENT_RUN_NUMBER ]
     then
         echo "existing_runs=true" >> "$GITHUB_OUTPUT"
