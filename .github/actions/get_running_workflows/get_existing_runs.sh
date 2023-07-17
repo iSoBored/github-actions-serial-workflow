066d46d65c7519ea57251b829b185bd15d3788c8
Nicholas <nichlai02@gmail.com>
2023-07-17 15:41:18 +0800

    fix loop

--- .github/actions/get_running_workflows/get_existing_runs.sh
+++ .github/actions/get_running_workflows/get_existing_runs.sh
@@ -1,7 +1,7 @@
 mapfile -t all_runs < all_runs.txt
 echo "existing_runs=false" >> "$GITHUB_OUTPUT"
 echo "Current Run Number: $CURRENT_RUN_NUMBER"
-for run_number in "$all_runs[@]"; do
+for run_number in "${all_runs[@]}"; do
     echo "For Loop Run Number: $run_number"
     if [ $run_number -lt $CURRENT_RUN_NUMBER ]
     then
