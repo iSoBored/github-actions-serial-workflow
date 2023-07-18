9494aab7b60022b44abbd5697c70e7918079cf58
Nicholas <nichlai02@gmail.com>
2023-07-18 17:30:19 +0800

    added section for potential issues

--- scripts/get_existing_runs.sh
+++ scripts/get_existing_runs.sh
@@ -1,7 +1,7 @@
 mapfile -t all_runs < all_runs.txt
 echo "existing_runs=false" >> "$GITHUB_OUTPUT"
 for run_number in "${all_runs[@]}"; do
-    if [ $run_number -lt $CURRENT_RUN_NUMBER ] # -lt to -ne 
+    if [ $run_number -lt $CURRENT_RUN_NUMBER ]
     then
         echo "existing_runs=true" >> "$GITHUB_OUTPUT"
     fi
