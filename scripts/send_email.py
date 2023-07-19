a0bd7717cf8322e0e949a9fe1a104f04f5b02dea
Nicholas <nichlai02@gmail.com>
2023-07-19 10:41:01 +0800

    added send email

---
+++ scripts/send_email.py
@@ -0,0 +1,13 @@
+import sendgrid
+import os
+
+print("SENDGRID_TOKEN:", os.getenv("SENDGRID_TOKEN"))
+sg = sendgrid.SendGridClient(os.getenv("SENDGRID_TOKEN"))
+message = sendgrid.Mail()
+
+print("PUSHER_EMAIL:", os.getenv("PUSHER_EMAIL"))
+message.add_to(os.getenv("PUSHER_EMAIL"))
+message.set_from("test@testemail.com")
+message.set_subject("Sending with SendGrid is Fun")
+message.set_html("and easy to do anywhere, even with Python")
+sg.send(message)
