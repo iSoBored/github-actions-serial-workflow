aaa9f764c643792a472fbd3c9308f23747b48ce3
Nicholas <nichlai02@gmail.com>
2023-07-21 13:40:52 +0800

    change condition

--- .github/actions/notify_pusher/send_email.py
+++ .github/actions/notify_pusher/send_email.py
@@ -2,15 +2,13 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-# sg = sendgrid.SendGridAPIClient(api_key=os.getenv("SENDGRID_TOKEN"))
-# from_email = Email(os.getenv("EMAIL_SENDER"))
-# to_email = To(os.getenv("PUSHER_EMAIL"))
-# subject = "Sending with SendGrid is Fun"
-# content = Content("text/plain", "and easy to do anywhere, even with Python")
-# mail = Mail(from_email, to_email, subject, content)
-# response = sg.client.mail.send.post(request_body=mail.get())
-# print(response.status_code)
-# print(response.body)
-# print(response.headers)
-
-print('test')
+sg = sendgrid.SendGridAPIClient(api_key=os.getenv("SENDGRID_TOKEN"))
+from_email = Email(os.getenv("EMAIL_SENDER"))
+to_email = To(os.getenv("PUSHER_EMAIL"))
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, to_email, subject, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response.status_code)
+print(response.body)
+print(response.headers)
